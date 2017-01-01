11622c1026ac341cab7fd4d2b10b25a6c695b6ee
r_takaishi <ryo.takaishi.0@gmail.com>
2017-01-01 11:29:53 +0900

    bundle gem

---
+++ lib/ruboty/random_hatenabookmark.rb
@@ -0,0 +1,7 @@
+require "ruboty/random_hatenabookmark/version"
+
+module Ruboty
+  module RandomHatenabookmark
+    # Your code goes here...
+  end
+end
---
+++ lib/ruboty/random_hatenabookmark/version.rb
@@ -0,0 +1,5 @@
+module Ruboty
+  module RandomHatenabookmark
+    VERSION = "0.1.0"
+  end
+end
